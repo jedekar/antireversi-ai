c21f284bbc194cd874603240023de7f3e4c6f094
Alex Khodos <ax.khodos@gmail.com>
2020-10-28 20:50:11 +0200

    Change style to camel case in main

--- src/main.nim
+++ src/main.nim
@@ -1,7 +1,7 @@
 import reversi, controller, runner
 
 let gameInfo = prepare()
-let game = newReversi(game_info[2])
-let gameRunner = newRunner(game_info[0], game_info[1])
+let game = newReversi(gameInfo[2])
+let gameRunner = newRunner(gameInfo[0], gameInfo[1])
 while not game.isFinished():
     gameRunner.process(game)
