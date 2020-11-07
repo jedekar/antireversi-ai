2cddf7f300ce61918b5ce635df912a91bf78ce68
Alex Khodos <ax.khodos@gmail.com>
2020-11-07 13:39:54 +0200

    Fix minimax

--- src/minimax.nim
+++ src/minimax.nim
@@ -1,6 +1,8 @@
 import sugar, sequtils
 import reversi
 
+const Infinity = 1000
+
 proc copyGameWithMovesMadeBy(game: Reversi, color: char): seq[Reversi] = 
     let moves = game.getAvailableMovesFor(color)
     for i in 0..<len(moves):
@@ -15,13 +17,6 @@ type MinimaxTree = ref object
 proc newMinimaxTree(): MinimaxTree =
     return MinimaxTree(value: -1, move: InvalidCell, body: @[])
 
-proc getMoveWithMaxValue*(trees: seq[MinimaxTree]): CellIndex = 
-    var value = 0
-    for i in 0..<len(trees):
-        if value < trees[i].value:
-            value = trees[i].value
-            result = trees[i].move
-
 proc minimax*(game: Reversi, color: char, depth: int): MinimaxTree =
     proc min(s: seq[MinimaxTree]): MinimaxTree =
         result = s[minIndex(map(s, (a) => a.value))]
@@ -29,19 +24,27 @@ proc minimax*(game: Reversi, color: char, depth: int): MinimaxTree =
     let inverse = inverseof(color)
     let tree = newMinimaxTree()
     let moves = game.getAvailableMovesFor(color)
-    if len(moves) == 0 or depth == 0:
+    if depth == 0 or len(moves) == 0:
+        tree.value = game.calculateScoreForColor(color)
         return tree
 
     let level = game.copyGameWithMovesMadeBy(color)
+
     for i in 0..<len(level):
         tree.body.add(minimax(level[i], inverse, depth-1))
         tree.body[i].move = moves[i]
-        if depth == 1:
-            tree.body[i].value = level[i].calculateScoreForColor(color)
-        else:
-            try:
-                tree.body[i].value = min(tree.body[i].body).value
-            except IndexError:
-                tree.body[i].value = 1000
+    
+    let negamax = if (depth mod 2) == 0: -1 else: 1
+    try:
+        tree.value = negamax * min(tree.body).value
+    except IndexError:
+        tree.value = negamax * Infinity
 
     return tree
+
+proc getMoveWithMaxValue*(trees: seq[MinimaxTree]): CellIndex = 
+    var value = Infinity
+    for i in 0..<len(trees):
+        if value > trees[i].value:
+            value = trees[i].value
+            result = trees[i].move
